c8390d1858aa41a28f216e45ae92016927f13e3c
SashaRoachakKapoor <121806975+SashaRoachakKapoor@users.noreply.github.com>
2023-09-15 20:27:50 +0400

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,9 @@
+import random
+response=='y'
+no=random.randit(1,6)
+print('[             ]')
+print('[             ]')
+print('[             ]')
+print('[             ]')
+print('[             ]')
+print('[             ]')
